1ccdb3635018899962b245007596423f5e3618f1
Larva Tech <46956076+larvacent@users.noreply.github.com>
2022-03-19 21:25:58 +0800

    Update .php-cs-fixer.php

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -1,19 +1,26 @@
 <?php
-
 return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
     ->setRules([
-        '@PSR12' => true,
+        '@PSR2' => true,
+        '@DoctrineAnnotation' => true,
         'binary_operator_spaces' => true,
+        'blank_line_before_statement' => [
+            'statements' => [
+                'declare',
+            ],
+        ],
         'blank_line_after_opening_tag' => true,
         'compact_nullable_typehint' => true,
         'declare_equal_normalize' => true,
         'lowercase_cast' => true,
         'lowercase_static_reference' => true,
         'new_with_braces' => true,
+        'no_unused_imports' => false,
         'no_blank_lines_after_class_opening' => true,
         'no_leading_import_slash' => true,
         'no_whitespace_in_blank_line' => true,
-        'no_unused_imports' => true,
+        'single_trait_insert_per_statement' => false,
         'ordered_class_elements' => [
             'order' => [
                 'use_trait',
@@ -21,16 +28,22 @@ return (new PhpCsFixer\Config())
         ],
         'ordered_imports' => [
             'imports_order' => [
-                'class',
-                'function',
-                'const',
+                'class', 'function', 'const',
             ],
             'sort_algorithm' => 'none',
+        ],
+        'list_syntax' => [
+            'syntax' => 'short'
+        ],
+        'phpdoc_align' => [
+            'align' => 'left',
+        ],
+        'constant_case' => [
+            'case' => 'lower',
         ],
         'return_type_declaration' => true,
         'short_scalar_cast' => true,
         'single_blank_line_before_namespace' => true,
-        'single_trait_insert_per_statement' => true,
         'ternary_operator_spaces' => true,
         'unary_operator_spaces' => true,
         'visibility_required' => [
@@ -40,10 +53,25 @@ return (new PhpCsFixer\Config())
                 'property',
             ],
         ],
-    ])
-    ->setFinder(
+        'combine_consecutive_unsets' => true,
+        'linebreak_after_opening_tag' => true,
+        'phpdoc_separation' => false,
+        'single_quote' => true,
+        'standardize_not_equals' => true,
+        'multiline_comment_opening_closing' => true,
+        'ternary_to_null_coalescing' => true,
+        //'declare_strict_types' => true,//激进，强制打开严格模式
+    ])->setFinder(
         PhpCsFixer\Finder::create()
+            ->exclude('bootstrap')
+            ->exclude('config')
+            ->exclude('database')
+            ->exclude('lang')
+            ->exclude('node_modules')
+            ->exclude('public')
+            ->exclude('storage')
             ->exclude('vendor')
-            ->in([__DIR__.'/src/', __DIR__.'/tests/'])
-    )
-;
+            ->exclude('resources')
+            ->exclude('routes')
+            ->in(__DIR__)
+    )->setUsingCache(false);
